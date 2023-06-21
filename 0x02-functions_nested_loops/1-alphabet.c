e49ee3416e56a1ad887336de3a7fcf094cc7b335
mmesomaadoro <adorojanefrances@gmail.com>
2023-06-20 17:30:02 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lowercases accompanied by a new line
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}	_putchar('\n');
+
+}
