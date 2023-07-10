af8f1d55aaac3bae7ad5b84a68913677fd97cea4
mmesomaadoro <adorojanefrances@gmail.com>
2023-07-10 13:58:17 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Check if the program received exactly two arguments
+ * Convert the arguments to integers
+ * Perform the multiplication
+ *  Print the result followed by a new line
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return 1;
+	}
+	
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	
+	return 0;
+}
