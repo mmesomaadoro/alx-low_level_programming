9ceb094f05a8f94fe82c5f3afbb4586b54cf5e4e
mmesomaadoro <adorojanefrances@gmail.com>
2023-07-29 04:33:32 -0700

    Trial

---
+++ Mmeso/Trial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - to print Hello_world
+ * printf - prints "Hello_world!"
+ * function main: variable condition
+ * Return:return to 0
+ */
+int main(void)
+{
+	printf("Hello_world!\n");
+	return (0);
+}
