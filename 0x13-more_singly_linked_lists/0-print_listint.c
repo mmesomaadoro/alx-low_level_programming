a502f7472e75603939a0a96108d9515131db23a8
mmesomaadoro <adorojanefrances@gmail.com>
2023-07-31 08:14:45 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t reg = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		count++;
+		reg++;
 		h = h->next;
 	}
-	return (count);
+	return (reg);
 }
 
